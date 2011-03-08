394704708c05adb1aa1225dfcc4d5123b47320bb
Chris Johnson <chris@johnsonch.com>
2011-03-08 08:29:35 -0600

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,97 @@
+set :user, 'CHANGE_ME_TO_YOUR_DREAMHOST_USER'
+set :application, "CHANGE_ME_TO_YOUR_SITE" 
+set :repository, "CHANGE_ME_TO_YOUR_REPOLOCATION" 
+set :password, "CHANGE_ME_TO_YOUR_DREAMHOST_PASSWORD_IF_YOU_DONT_HAVE_SSH_KEYS"
+set :scm, :git
+
+# =============================================================================
+# You shouldn't have to modify the rest of these
+# =============================================================================
+
+role :web, application
+role :app, application
+role :db,  application, :primary => true
+
+set :deploy_to, "/home/#{user}/#{application}" 
+# set :svn, "/path/to/svn"       # defaults to searching the PATH
+set :use_sudo, false
+# set :restart_via, :run
+
+
+# saves space by only keeping last 3 when running cleanup
+set :keep_releases, 3 
+
+# issues svn export instead of checkout
+set :checkout, "export" 
+set :copy_strategy, :export
+
+# keeps a local checkout of the repository on the server to get faster deployments
+#set :deploy_via, :copy
+
+ssh_options[:paranoid] = false
+
+# =============================================================================
+# OVERRIDE TASKS
+# =============================================================================
+namespace :deploy do
+    desc "Restart Passenger" 
+    task :restart, :roles => :app do
+      run "touch #{current_path}/tmp/restart.txt" 
+    end
+  
+      task :after_update_code, :roles => :app do 
+          #rcov messes with deployed apps...
+          run "rm -rf #{release_path}/vendor/plugins/rails_rcov" 
+        end
+
+    desc <<-DESC
+      Deploy and run pending migrations. This will work similarly to the \
+      `deploy' task, but will also run any pending migrations (via the \
+      `deploy:migrate' task) prior to updating the symlink. Note that the \
+      update in this case it is not atomic, and transactions are not used, \
+      because migrations are not guaranteed to be reversible.
+    DESC
+  
+
+    task :migrations do
+      set :migrate_target, :latest
+      update_code
+      symlink
+      migrate
+      restart
+    end
+
+  end
+
+  db_params = {
+    "adapter"=>"mysql",
+    "database"=>"CHANGE_ME_TO_YOUR_DATABASE",
+    "username"=>"CHANGE_ME_TO_YOUR_USERNAME",
+    "password"=>"CHANGE_ME_TO_YOUR_PASSWORD",
+    "host"=>"mysql.CHANGE_ME_TO_YOUR_SITE.com",
+    "socket"=>""
+  }
+
+  task :my_generate_database_yml, :roles => :app do
+    database_configuration = "production:\n"
+    db_params.each do |param, default_val|
+      database_configuration<<"  #{param}: #{default_val}\n"
+    end
+    run "mkdir -p #{deploy_to}/#{shared_dir}/config"
+    put database_configuration, "#{release_path}/config/database.yml"
+  end
+  after "deploy:symlink", :my_generate_database_yml
+  
+
+
+
+
+  ##/public/user_uploads/cms/images
+  task :symlink_uploads do
+    # run "mkdir -p #{deploy_to}/#{current_dir}/public/user_uploads/"
+    run "ln -s #{deploy_to}/#{shared_dir}/user_uploads #{deploy_to}/#{current_dir}/public/"
+  end  
+  after "deploy:symlink", :symlink_uploads
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamhostDemo::Application.config.secret_token = 'ccce09ca29857eee6dca6fbfa06d74d8f639e08a314615693c208bcb14cd2a68b2fcd83ba89a7b771377bd1fdbf3993e89f869007cde95c051bf71e9b76e4ced'
